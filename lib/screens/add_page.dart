69e45358c13abf094b3c19d7608dd0ce85eb3a61
Anil Antony <anilvithayathil007@gmail.com>
2024-05-14 17:42:19 +0530

    post complete

--- lib/screens/add_page.dart
+++ lib/screens/add_page.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class AddTodoPage extends StatefulWidget {
   const AddTodoPage({super.key});
@@ -8,6 +11,9 @@ class AddTodoPage extends StatefulWidget {
 }
 
 class _AddTodoPageState extends State<AddTodoPage> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,12 +24,14 @@ class _AddTodoPageState extends State<AddTodoPage> {
         padding: EdgeInsets.all(20),
         children: [
           TextFormField(
+            controller: titleController,
             decoration: InputDecoration(hintText: 'Title'),
           ),
           SizedBox(
             height: 20,
           ),
           TextFormField(
+            controller: descriptionController,
             decoration: const InputDecoration(
               hintText: 'Description',
             ),
@@ -34,9 +42,50 @@ class _AddTodoPageState extends State<AddTodoPage> {
           SizedBox(
             height: 20,
           ),
-          ElevatedButton(onPressed: () {}, child: Text('Submit'))
+          ElevatedButton(onPressed: submitData, child: Text('Submit'))
         ],
       ),
     );
+  }
+
+  Future<void> submitData() async {
+    final title = titleController.text;
+    final description = descriptionController.text;
+    final body = {
+      "title": title,
+      "descriptiion": description,
+      "is_completed": false,
+    };
+
+    final url = "https://api.nstack.in/v1/todos";
+    final uri = Uri.parse(url);
+    final response = await http.post(uri,
+        body: jsonEncode(body), headers: {'Content-Type': 'application/json'});
+
+    if (response.statusCode == 201) {
+      titleController.text = '';
+      descriptionController.text = '';
+      // print('Success');
+      showSuccsessMessage('Todo added Successfully');
+    } else {
+      // print('failed');
+      showErrorMessage(response.body);
+    }
+
+    // print(response.statusCode);
+  }
+
+  void showSuccsessMessage(String message) {
+    final snackBar = SnackBar(content: Text(message));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  void showErrorMessage(String message) {
+    final snackBar = SnackBar(
+        content: Text(
+      message,
+      style: TextStyle(color: Colors.red),
+    ));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
